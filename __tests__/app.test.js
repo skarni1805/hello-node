11fae51868094b07985c4c8115202e2fa01702ef
Jon Keam <jpkeam@gmail.com>
2022-08-27 16:47:40 -0400

    update test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,12 @@ const app = require('../app');
 const request = require('supertest');
 const baseURL = "http://localhost:8080";
 
-describe('GET /', () => {
+describe('App', () => {
   afterAll(() => {
     app.close();
   });
 
-  it('should return success', async () => {
+  it('should return success when hitting root', async () => {
     const response = await request(baseURL).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.body.error).toBe(undefined);
